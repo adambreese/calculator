363fd648d44c5d73d8f797acc352c9c2db4d5c20
Adam <14831693+adambreese@users.noreply.github.com>
2024-02-27 13:46:07 -0500

    Fix all functions with log statements

--- calc.js
+++ calc.js
@@ -1,51 +1,55 @@
-let num1;
-let num2;
-let operator;
 
-const operate = function(num1, num2, operator) {
+
+const operate = function(firstNum, secondNum, operator) {
     switch (operator) {
         case "add":
-            add(num1, num2);
+            console.log(add(firstNum, secondNum));
             break;
         case "subtract":
-            subtract(num1, num2);
+            console.log(subtract(firstNum, secondNum));
             break;
         case "multiply":
-            multiply(num1, num2);
+            console.log(multiply(firstNum, secondNum));
             break;
         case "divide":
-            divide(num1, num2);
+            console.log(divide(firstNum, secondNum));
             break;
         case "power":
-            power(num1, num2);
+            console.log(power(firstNum, secondNum));
+            break;
     }
-}
+};
 
 const add = function(num1, num2) {
     const addThese = [num1, num2];
     return addThese.reduce((total, currentItem) => total + currentItem, 0);
-  };
+};
   
-  const subtract = function(num1, num2) {
+const subtract = function(num1, num2) {
     const subtractThese = [num1, num2];
     return subtractThese.reduce((total, currentItem) => total - currentItem);
-  };
+};
   
-  const multiply = function(num1, num2) {
+const multiply = function(num1, num2) {
     const multiplyThese = [num1, num2];
     return multiplyThese.reduce((total, currentItem) => total * currentItem, 1);
-  };
+};
 
-  const divide = function(num1, num2) {
-    const divideThese = [num1, num2];
-    return divideThese.reduce((total, currentItem) => total / currentItem, array[0]);
-  };
+const divide = function(num1, num2) {
+    if (num2 != 0) {
+        const divideThese = [num1, num2];
+    return divideThese.reduce((total, currentItem) => total / currentItem, power(divideThese[0],2));
+    }
+    else {
+        return "Nice try! But there are infinite reasons not to divide by zero.";
+    }
+};
   
-  const power = function(num, power) {
+const power = function(num, power) {
     let powerUp = num;
     while (power-1) {
       powerUp = powerUp * num;
       power--;
     }
     return powerUp;
-  };
+};
