34338718d1c74e5fe4c9238f5bbd6c6d26a3fc0a
Adam <14831693+adambreese@users.noreply.github.com>
2024-02-27 13:23:08 -0500

    Add starting variables and operate function

--- calc.js
+++ calc.js
@@ -1,3 +1,26 @@
+let num1;
+let num2;
+let operator;
+
+const operate = function(num1, num2, operator) {
+    switch (operator) {
+        case "add":
+            add(num1, num2);
+            break;
+        case "subtract":
+            subtract(num1, num2);
+            break;
+        case "multiply":
+            multiply(num1, num2);
+            break;
+        case "divide":
+            divide(num1, num2);
+            break;
+        case "power":
+            power(num1, num2);
+    }
+}
+
 const add = function(num1, num2) {
     const addThese = [num1, num2];
     return addThese.reduce((total, currentItem) => total + currentItem, 0);
@@ -8,12 +31,14 @@ const add = function(num1, num2) {
     return subtractThese.reduce((total, currentItem) => total - currentItem);
   };
   
-  const sum = function(array) {
-      return array.reduce((total, currentItem) => total + currentItem, 0);
+  const multiply = function(num1, num2) {
+    const multiplyThese = [num1, num2];
+    return multiplyThese.reduce((total, currentItem) => total * currentItem, 1);
   };
-  
-  const multiply = function(array) {
-    return array.reduce((total, currentItem) => total * currentItem, 1);
+
+  const divide = function(num1, num2) {
+    const divideThese = [num1, num2];
+    return divideThese.reduce((total, currentItem) => total / currentItem, array[0]);
   };
   
   const power = function(num, power) {
